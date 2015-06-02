8954a3c77048e5e4743d99a2bdf07cad6b531c0a
blah103 <djordje_7@hotmail.com>
2015-06-01 23:21:59 -0400

    read from file hard link open

---
+++ src/fun.py
@@ -0,0 +1,42 @@
+from collections import Counter
+from random import randrange
+import math
+def main():
+    count = 0;
+    dict = {"a":1,"b":2,"c":2}
+    print(dict)
+    a = [1,2,3,4,5]
+    for x in range(1,5):
+        print("{}".format(x))
+    #for key in dict.keys():
+    #    print(dict[key])
+    prime = True
+    for x in range(2,3):
+        print(x)
+    N = int(input())
+    for i in range(2,N):
+        print("Integer: {}\n".format(i))
+        for x in range(2,math.ceil(math.sqrt(i))+1):
+            prime = True
+            print("Testing prime on: {}\n".format(x))
+            if(i%x==0 and i!=x):
+                prime = False
+                break 
+        if prime == True:
+            print("Prime: {}".format(i))
+            count+=1
+   # print(count)
+#   randn = [randrange(10) for i in range(100)]
+#   print(len(randn))
+#   cntr = Counter()
+#   for i in randn:
+#       cntr[i]+=1
+#   print(cntr)
+#   print (cntr[0])
+
+
+
+  #dict = {key: value for key,value in enumerate(cntr)}
+  #print (dict)
+if __name__ == "__main__":
+    main()
---
+++ src/main/app.py
@@ -0,0 +1,59 @@
+import urllib.request
+import re
+import webbrowser
+
+"""                       """
+""" Classes and functions """"""                       """
+class Manga:
+    def __init__(self,title,nc = 1 ):
+        self.title = title
+        self.nc = int(nc)
+        self.alert = True
+        self.lastread = int(0)  
+
+def list_init():
+    try:
+        file = open("C:\\Users\\djord_000\\Desktop\\Djordje\\Software\\Eclipse Workspace\\intro\\mangas.txt",'r+')
+        mlist = []
+        lines = file.readlines()
+        for line in lines:
+            mlist.append(Manga(line.split(' ')[0],line.split(' ')[1]))
+        return mlist   
+    except IOError:
+        print("IOError couldn't open mangas.txt\n")
+    
+       
+def newchaptercheck(manga):
+    html_content = urllib.request.urlopen('http://www.mangareader.net/{0}'.format(manga.title)).read()
+    matches = re.findall(str(manga.nc),str(html_content))
+    if len(matches) != 0:
+        webbrowser.open("http://www.mangareader.net/{0}/{1}".format(manga.title,str(manga.nc)))
+    else:
+        print("Error searching for manga couldn't find newest chapter")
+
+    
+def refresh(mlist):
+    for manga in mlist:
+        html_content = urllib.request.urlopen('http://www.mangareader.net/{0}'.format(manga.title)).read()
+        while(len(re.findall(str(manga.nc+1),str(html_content)))>0):
+            manga.nc+=1
+        if(manga.nc>manga.lastread):
+            manga.alert = True
+
+"""                       """
+"""                       """
+"""                       """
+
+mlist = list_init()
+newchaptercheck (mlist[0])
+#refresh(mlist)
+
+for x in mlist:
+    print("{} {}\n".format(x.title,x.nc))
+#    
+# shingeki = Manga("shingeki-no-kyojin",63)
+# a.append(shingeki)
+# for x in a:
+#     newchaptercheck(x)
+print(("a","b"))
+x = input('Gimme da money: ')
