dee1958f8b3b2712cb6106fb5b4522f6771fd216
blah103 <djordje_7@hotmail.com>
2015-06-06 12:02:09 -0400

    wrong newest chapter because it crawls html source, added title to html formatting, need to add reverse to make source searchable

--- src/app.py
+++ src/app.py
@@ -2,79 +2,69 @@ import urllib.request
 import re
 import webbrowser
 import os, sys
+import pprint
 
+# refresh(mlist)
 
-#refresh(mlist)
-
-#for x in mlist:
+# for x in mlist:
 #    print("{} {}\n".format(x.title,x.nc))
 #    
 # shingeki = Manga("shingeki-no-kyojin",63)
 # a.append(shingeki)
 # for x in a:
 #     newchaptercheck(x)
-#x = input('Gimme da money: ')
+# x = input('Gimme da money: ')
 
 class Manga:
-    def __init__(self,title,lastread =1, newestchap=1 ):
+    def __init__(self, title, lastread=1, newestchap=1):
         self.title = title
         self.newestchap = newestchap
         self.alert = True
-        self.lastread = lastread  
+        self.lastread = lastread
+
 
 def dict_init():
-	try:
-		dir = os.getcwd()
-		filename = dir + '/mangas.txt'
-		file = open(filename,'r+')
-		mangadict = {}
-		lines = file.readlines()
-		for line in lines:
-			title = line.split(' ')[0]
-			lastread = int(line.split(' ')[1])
-			print("%s" % title)
-			mangadict[title] = Manga(title=title,lastread=lastread)
-		return mangadict
-	except IOError:
-        	print("IOError couldn't open mangas.txt\n")
-    
+    try:
+        dir = os.getcwd()
+        filename = dir + '/mangas.txt'
+        file = open(filename, 'r+')
+        mangadict = {}
+        lines = file.readlines()
+        for line in lines:
+            title = line.split(' ')[0]
+            lastread = int(line.split(' ')[1])
+            print("%s" % title)
+            mangadict[title] = Manga(title=title, lastread=lastread)
+        return mangadict
+    except IOError:
+        print("IOError couldn't open mangas.txt\n")
 
 
-def list_init():
-	try:
-		dir = os.getcwd()
-		filename = dir + '/mangas.txt'
-		file = open(filename,'r+')
-		mangalist = []
-		lines = file.readlines()
-		for line in lines:
-			mlist.append(Manga(line.split(' ')[0],line.split(' ')[1]))
-		return mangalist
-	except IOError:
-		print("IOError couldn't open mangas.txt\n")
-    
-       
 def newchapteropen(manga):
-	refresh(manga)
-	if manga.lastread < manga.newestchap:
-		try:
-			webbrowser.open("http://www.mangareader.net/{0}/{1}".format(manga.title,str(manga.lastread+1)))
-			manga.lastread +=1
-		except Exception as e:
-			print("Couldn't open the new chapter! Exception: %s" % e)
-	else:
-		print("No new chapters sorry bud!")
+    refresh(manga)
+    if manga.lastread < manga.newestchap:
+        try:
+            webbrowser.open("http://www.mangareader.net/{0}/{1}".format(manga.title, str(manga.lastread + 1)))
+            manga.lastread += 1
+        except Exception as e:
+            print("Couldn't open the new chapter! Exception: %s" % e)
+    else:
+        print("No new chapters sorry bud!")
 
-    
+
 def refresh(manga):
-	print("http://www.mangareader.net/%s" % manga.title)
-	html_content = urllib.request.urlopen('http://www.mangareader.net/{0}'.format(manga.title)).read()
-	while(len(re.findall(str(manga.newestchap+1),str(html_content)))>0):
-		manga.newestchap+=1
-		if(manga.newestchap>manga.lastread):
-			manga.alert = True
+    print("http://www.mangareader.net/%s" % manga.title)
+    html_content = urllib.request.urlopen('http://www.mangareader.net/{0}'.format(manga.title)).read()
+    while (len(re.findall(pattern=str(manga.newestchap + 1), string=str(html_content))) > 0):
+        manga.newestchap += 1
+        if (manga.newestchap > manga.lastread):
+            manga.alert = True
+
+def titleFormat(title):
+    return re.sub(pattern='\ ',repl='-',string=title.lower())
+
 
 mangadict = dict_init()
 newchapteropen(mangadict["bleach"])
 
-print("{} {}\n".format(mangadict["bleach"].title,int(mangadict["bleach"].newestchap)))
+print("{} {}\n".format(mangadict["bleach"].title, int(mangadict["bleach"].newestchap)))
--- src/fun.py
+++ src/fun.py
@@ -1,30 +1,40 @@
 from collections import Counter
 from random import randrange
 import math
+import re
 def main():
-    count = 0;
-    dict = {"a":1,"b":2,"c":2}
-    print(dict)
-    a = [1,2,3,4,5]
-    for x in range(1,5):
-        print("{}".format(x))
-    #for key in dict.keys():
-    #    print(dict[key])
-    prime = True
-    for x in range(2,3):
-        print(x)
-    N = int(input())
-    for i in range(2,N):
-        print("Integer: {}\n".format(i))
-        for x in range(2,math.ceil(math.sqrt(i))+1):
-            prime = True
-            print("Testing prime on: {}\n".format(x))
-            if(i%x==0 and i!=x):
-                prime = False
-                break 
-        if prime == True:
-            print("Prime: {}".format(i))
-            count+=1
+
+    str = "Shingeki No Kyojin"
+    print(str)
+    strlower = str.lower()
+    print(strlower)
+    finalstr = re.sub(pattern='\ ',repl='-',string=strlower)
+    print(finalstr)
+
+
+    # count = 0;
+    # dict = {"a":1,"b":2,"c":2}
+    # print(dict)
+    # a = [1,2,3,4,5]
+    # for x in range(1,5):
+    #     print("{}".format(x))
+    # #for key in dict.keys():
+    # #    print(dict[key])
+    # prime = True
+    # for x in range(2,3):
+    #     print(x)
+    # N = int(input())
+    # for i in range(2,N):
+    #     print("Integer: {}\n".format(i))
+    #     for x in range(2,math.ceil(math.sqrt(i))+1):
+    #         prime = True
+    #         print("Testing prime on: {}\n".format(x))
+    #         if(i%x==0 and i!=x):
+    #             prime = False
+    #             break
+    #     if prime == True:
+    #         print("Prime: {}".format(i))
+    #         count+=1
    # print(count)
 #   randn = [randrange(10) for i in range(100)]
 #   print(len(randn))
